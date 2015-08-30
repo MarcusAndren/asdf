443faa794d3d9286453a79b319417ab6b66c2ce2
MarcusAndren <andren.marcus@gmail.com>
2015-08-30 22:53:50 +0200

    updated FN layers with media keys and and added win/mac key and fixed FN keys, removed unused keys

--- keymap_asdf.c
+++ keymap_asdf.c
@@ -4,19 +4,26 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
   KEYMAP(ESC,  1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,DEL, 	\
          TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,ENT, PGUP, \
-         FN2,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,TRNS,PGDN, \
+         FN0,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,TRNS,PGDN, \
          LSFT, NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,UP, 	HOME, \
-         LCTL, FN0, LALT,TRNS,TRNS,SPC, TRNS,TRNS,TRNS,RALT,RCTL,FN1, LEFT,DOWN,RIGHT),
+         LCTL, LGUI,LALT,					 SPC, 							 RALT,RCTL,FN1, LEFT,DOWN,RIGHT),
 
     /* 1: FN 1 */
-    KEYMAP(GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  DEL, 1, \
-           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,1, \
-           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MYCM, TRNS, TRNS, TRNS, TRNS, RGHT, TRNS, ENT, 1, \
-           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PGUP, TRNS,1, \
-           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, PGDN, END, 1),
+    KEYMAP(GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  DEL, MUTE, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,MNXT, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, ENT, MPRV, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, UP  ,MPLY, \
+           TRNS, TRNS, TRNS,			 			 TRNS, 									 TRNS, TRNS, TRNS, LEFT, DOWN,RIGHT),
+	
+    /* 1: FN 2 */
+    KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, VOLU, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, VOLD, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, TRNS, TRNS, 						 TRNS,									 TRNS, TRNS, TRNS, TRNS, TRNS, TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),
-  [1] = ACTION_LAYER_TAP_KEY(1, KC_ESC),
+  [1] = ACTION_LAYER_MOMENTARY(2),
 };
